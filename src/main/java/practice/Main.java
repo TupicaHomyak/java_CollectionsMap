8733105ddfa9c5b4b46008109708960f9c8bc0a7
Olga Malkova <Vodeviiil@mail.ru>
2023-06-20 19:47:25 +0300

    11.9 практ задание с правкой

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -5,12 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PhoneBook phoneBook = new PhoneBook(); //хранениe контактов
-        while (true) {
-            //основная логика программы
+        while (true) {//основная логика программы
             System.out.println("Введите номер, имя или команду:");
-            String input = scanner.nextLine().trim();
-            //Если введена команда "LIST", вызывается метод getAllContacts
-            // объекта PhoneBook для получения всех контактов, которые выводятся на экран
+            String input = scanner.nextLine().trim();//вызывается метод getAllContacts
             if (input.equalsIgnoreCase("LIST")) {
                 Set<String> allContacts = phoneBook.getAllContacts();
                 if (allContacts.isEmpty()) {
@@ -21,9 +18,7 @@ public class Main {
                     }
                 }
             } else {//Если введено пустое значение, цикл продолжается снова.
-                if (input.isEmpty()) {
-                    continue;
-                }
+                if (input.isEmpty()) { continue;}
                 if (input.matches("\\d+")) {//Если введен номер, вызывается getContactByPhone
                     String contact = phoneBook.getContactByPhone(input);
                     if (contact.isEmpty()) {//контакт не найден, запрашивается имя  и вызывается  addContact
@@ -31,32 +26,25 @@ public class Main {
                         System.out.print("Введите имя абонента для номера \"" + input + "\": ");
                         String name = scanner.nextLine().trim();
                         phoneBook.addContact(input, name);
-                        System.out.println("Контакт сохранен!");
-                        //добавление контакта с  номером и именем
+                        System.out.println("Контакт сохранен!");//добавление контакта с  номером и именем
                     } else {
                         System.out.println(contact);//Если контакт найден, он выводится на экран.
                     }
-                    //Если введено имя, вызывается getContactByName  для поиска контакта по имени.
-                } else if (input.matches("[a-zA-Zа-яА-Я]+")) {
-                    Set<String> contacts = phoneBook.getContactByName(input);
+                } else if (input.matches("[a-zA-Zа-яА-Я]+")) {//Если введено имя,
+                    Set<String> contacts = phoneBook.getContactByName(input);//вызывается getContactByName
                     if (contacts.isEmpty()) {//контакт не найден, запраш. номер телефона абонента
                         System.out.println("Такого имени в телефонной книге нет.");
                         System.out.print("Введите номер телефона для абонента \"" + input + "\": ");
                         String phone = scanner.nextLine().trim();
-                        //вызывается метод addContact объекта PhoneBook
-                        // для добавления нового контакта с указанным именем и номером.
-                        phoneBook.addContact(phone, input);
+                        phoneBook.addContact(phone, input);//вызывается метод addContact объекта PhoneBook
                         System.out.println("Контакт сохранен!");
-                        //Если контакт найден, он выводится на экран.
                     } else {
                         for (String contact : contacts) {
-                            System.out.println(contact);
+                            System.out.println(contact);//Если контакт найден, он выводится на экран.
                         }
                     }
-                    //Если введенная строка не соответствует ни одному из вышеперечисленных условий,
-                    // выводится сообщение о неверном формате ввода.
-                } else {
-                    System.out.println("Неверный формат ввода");
+                } else {//Если введенная строка не соответствует ни одному из вышеперечисленных условий,
+                    System.out.println("Неверный формат ввода");// выводится сообщение о неверном формате ввода.
                 }
             }
         }
